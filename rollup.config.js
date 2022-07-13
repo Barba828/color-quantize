510ac0c894935078dd2b832671c5ac3c8412938d
Barba Lee <1435315192@qq.com>
2022-07-13 22:34:18 +0800

    feat: build configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+
+const isDev = process.env.NODE_ENV !== "production";
 
 export default [
   {
+    // dist 文件
     input: "src/index.ts",
-    plugins: [typescript({ jsx: "preserve" })],
+    plugins: [
+      typescript({ jsx: "preserve" }), // ts 编译
+      !isDev && terser(), // terser 压缩（prod模式下）
+    ],
     output: {
       file: "dist/bundle.js",
       format: "esm",
